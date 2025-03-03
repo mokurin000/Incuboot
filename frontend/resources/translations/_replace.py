64471382f6f1f60e23fb0044c9716349f5823e3d
mokurin000 <1348292515a@gmail.com>
2025-03-03 14:03:16 +0800

    chore: replace text in translations

---
+++ frontend/resources/translations/_replace.py
@@ -0,0 +1,23 @@
+import json
+from os import listdir
+
+
+def process(d: dict):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            process(v)
+        if isinstance(v, str):
+            while "AppFlowy" in v:
+                v = v.replace("AppFlowy", "GNY")
+            d[k] = v
+
+
+for json_file in listdir():
+    if not json_file.endswith(".json"):
+        continue
+
+    with open(json_file, "r", encoding="utf-8") as fp:
+        d: dict = json.load(fp)
+    process(d)
+    with open(json_file, "w", encoding="utf-8") as fp:
+        json.dump(d, fp, indent=4, ensure_ascii=False)
